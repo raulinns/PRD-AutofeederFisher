bb624ea72ffd7e6330edf64e9106afa05ed2aa86
Alfonzo <reinhardalfonso@gmail.com>
2025-06-08 22:47:55 +0700

    fix : User Interface

--- code/Code/Code.ino
+++ code/Code/Code.ino
@@ -1,74 +1,98 @@
-#include "FirebaseESP8266.h"
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
-#include <Servo.h>
+#include <ESP32Servo.h>
 
-Servo servo;
-
-#define FIREBASE_HOST "https://automaticfish-feeder-default-rtdb.asia-southeast1.firebasedatabase.app/"
+// Firebase config
+#define FIREBASE_HOST "automaticfish-feeder-default-rtdb.asia-southeast1.firebasedatabase.app"
 #define FIREBASE_AUTH "jzaLdSi2XrGgoUpI7lLPkPamNuYWf1VGWW6SXXEy"
 #define WIFI_SSID "sabeink"
 #define WIFI_PASSWORD "kuncengkempus123"
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org",25200);
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 25200); // GMT+7
 
+Servo servo;
 
-FirebaseData timer,feed;
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+String Str[] = {"00:00", "00:00", "00:00"};
 String stimer;
-String Str[]={"00:00","00:00","00:00"};
-int i,feednow=0;
+int i;
+int feednow = 0;
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to a");
+  Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
-  Serial.println();
-  Serial.print("connected: ");
+  Serial.println("\nConnected with IP: ");
   Serial.println(WiFi.localIP());
 
+  // Mulai sinkronisasi waktu
   timeClient.begin();
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  while (!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+
+  // Konfigurasi Firebase
+  config.database_url = FIREBASE_HOST;
+  config.signer.tokens.legacy_token = FIREBASE_AUTH;
+  config.cert.data = NULL;  // Matikan SSL certificate (wajib agar stabil)
+  config.time_zone = 7;     // WIB
+
+  Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
-  servo.attach(D3); // Pin attached to D3
+
+  servo.attach(13); // GPIO13 untuk sinyal servo
 }
 
 void loop() {
-  Firebase.getInt(feed,"feednow");
-  feednow = feed.to<int>();
-  Serial.println(feednow);
-  if (feednow==1) // Direct Feeding
-  {
-      servo.writeMicroseconds(1000); // rotate clockwise
-      delay(700); // allow to rotate for n micro seconds, you can change this to your need
-      servo.writeMicroseconds(1500); // stop rotation
-      feednow = 0;
-      Firebase.setInt(feed, "/feednow",feednow);
-      Serial.println("Fed");
-  }
-  else{ // Scheduling feed
-    for(i=0;i<3;i++){
-      String path = "timers/timer"+String(i);
-      Firebase.getString(timer,path);
-      stimer = timer.to<String>();
-      Str[i]= stimer.substring(9,14);
-    }
-    timeClient.update();
-    String currentTime = String(timeClient.getHours())+":"+String(timeClient.getMinutes());
-    if (Str[0]==currentTime||Str[1]==currentTime||Str[2]==currentTime)
-    {
-      servo.writeMicroseconds(1000); // rotate clockwise
-      delay(700); // allow to rotate for n micro seconds, you can change this to your need
-      servo.writeMicroseconds(1500); // stop rotation
-      Serial.println("Success");
-      delay(60000);
+  // Feed Now
+  if (Firebase.RTDB.getInt(&fbdo, "/feednow")) {
+    feednow = fbdo.intData();
+    Serial.println("feednow: " + String(feednow));
+    if (feednow == 1) {
+      servo.write(90);  // putar penuh
+      delay(700);
+      servo.write(0);    // kembali ke awal
+      Firebase.RTDB.setInt(&fbdo, "/feednow", 0);
+      Serial.println("Fed (manual)");
     }
   }
-  Str[0]="00:00";
-  Str[1]="00:00";
-  Str[2]="00:00";
+
+  // Jadwal Timer
+  for (i = 0; i < 3; i++) {
+    String path = "/timers/timer" + String(i) + "/time";
+    if (Firebase.RTDB.getString(&fbdo, path)) {
+      stimer = fbdo.stringData();
+      Str[i] = stimer.substring(0, 5); // "08:00"
+    } else {
+      Str[i] = "00:00";
+    }
+  }
+
+  timeClient.update();
+  String h = String(timeClient.getHours());
+  String m = String(timeClient.getMinutes());
+  if (h.length() == 1) h = "0" + h;
+  if (m.length() == 1) m = "0" + m;
+  String currentTime = h + ":" + m;
+  Serial.println("Now: " + currentTime);
+
+  if (Str[0] == currentTime || Str[1] == currentTime || Str[2] == currentTime) {
+    servo.write(90);
+    delay(700);
+    servo.write(0);
+    Serial.println("Fed (scheduled)");
+    delay(60000); // tunggu 1 menit
+  }
+
+  delay(1000);
 }
